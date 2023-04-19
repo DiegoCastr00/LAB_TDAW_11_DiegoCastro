b2d891ae7bc1411b7ec289a27c20419999f988cb
Dante <willd1530@gmail.com>
2023-04-18 22:44:28 -0600

    actualizacion

--- ejercicio02/ejercicio02.js
+++ ejercicio02/ejercicio02.js
@@ -1,27 +1,60 @@
 const imagenes = document.querySelectorAll('.imagen');
+const sectionnn = document.querySelectorAll('.nueva_img');
+
+var filtro = document.getElementById("filtro");
+var tabla = document.getElementById("tabla");
+// Agregar un evento change al elemento select
+filtro.addEventListener("change", function() {
+  // Obtener el valor seleccionado del elemento select
+  var valorFiltro = filtro.value;
+  console.log(valorFiltro)
+  // Recorrer todas las filas de la tabla
+  for (var i = 0; i < tabla.rows.length; i++) {
+    var fila = tabla.rows[i];
+    console.log(fila)
+      var valorCelda = fila.cells[5].innerHTML;
+    console.log(valorCelda)
+
+    // Ocultar o mostrar la fila dependiendo del valor seleccionado
+      if (valorFiltro == "Género" || valorFiltro === valorCelda ) {
+        
+      console.log(valorCelda)
+
+      fila.style.display = "";
+    } else {
+      fila.style.display = "none";
+    }
+  }
+});
+
+
+
+
+
+
+
+
+
+
+function copiasrc(imagenes,nuevaImagen){
+  const src = imagenes.src;
+  nuevaImagen.src = src;
+}
 
 for (let i = 0; i < imagenes.length; i++) {
+imagenes[i].addEventListener('mouseover', function() {
+  const nuevaImagen = document.createElement('img');
+  const sectionimg = document.createElement('section');
+  nuevaImagen.className = 'nuevaImagen';
+  sectionimg.className = 'secimg';
+  copiasrc(this,nuevaImagen)
 
-    imagenes[i].addEventListener('mouseenter', function() {
-        const src = this.src;
-        const nuevaImagen = document.createElement('img');
-        nuevaImagen.src = src;
-        contenedorNuevaImagen = document.createElement('div');
-        contenedorNuevaImagen.classList.add('contenedor-nueva-imagen');
-        
-        contenedorNuevaImagen.appendChild(nuevaImagen);
-        this.appendChild(contenedorNuevaImagen)
-
-        contenedorNuevaImagen.style.width ="20%"
-
-        this.parentNode.insertBefore(contenedorNuevaImagen.firstChild, this.nextSibling);
-        
-        this.addEventListener('mouseleave', function() {
-          const contenedor = document.querySelector('.contenedor-nueva-imagen');
-          contenedor.remove();
-        });
-      });
+  sectionimg.appendChild(nuevaImagen)
+  this.parentElement.appendChild(sectionimg)
 
 
-    
+  this.addEventListener('mouseleave', function() {
+    sectionimg.remove();
+  });
+});
 }
